d4417e4fc3b794c25a01fac4707f3887d7722fe4
spkchalla <shanmukhapadmakumar.challa@gmail.com>
2026-07-10 20:26:29 +0530

    2026-07-10

--- backend/server.js
+++ backend/server.js
@@ -52,7 +52,7 @@ app.get('/', (req, res) => {
     res.send(`Server running at ${PORT}`);
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server started running at port ${PORT}`);
 });
